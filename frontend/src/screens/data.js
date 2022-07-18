f6b72d927c77a2655b795145d3646341707ef7ec
Tetradodeca <urabummer4@hotmail.com>
2022-07-19 00:02:57 +0800

    created node express server

---
+++ frontend/src/screens/data.js
@@ -0,0 +1,57 @@
+const data = {
+  products: [
+    {
+      name: 'Nike Slim Shirt',
+      slug: 'nike-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p1.jpeg', // 679px x 829px
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High quality slim shirt',
+    },
+
+    {
+      name: 'Adidas Slim Shirt',
+      slug: 'adidas-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p2.jpeg',
+      price: 100,
+      countInStock: 9,
+      brand: 'Adidas',
+      rating: 4.8,
+      numReviews: 10,
+      description: 'High quality slim shirt',
+    },
+
+    {
+      name: 'Nike Slim Pant',
+      slug: 'nike-slim-pant',
+      category: 'Pants',
+      image: '/images/p3.jpeg',
+      price: 80,
+      countInStock: 15,
+      brand: 'Nike',
+      rating: 4.3,
+      numReviews: 10,
+      description: 'Dri fit tapered pants',
+    },
+
+    {
+      name: 'Adidas Slim Pant',
+      slug: 'adidas-slim-pant',
+      category: 'Pants',
+      image: '/images/p4.jpeg',
+      price: 180,
+      countInStock: 20,
+      brand: 'Adidas',
+      rating: 4.9,
+      numReviews: 3,
+      description: 'Fast selling Messi endorsed pants',
+    },
+  ],
+};
+
+export default data;
